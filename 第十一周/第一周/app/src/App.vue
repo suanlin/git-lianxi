ab5f7f0cbb334334f6c615f8e15e0ffad05ffe8d
suanlin <1402879619@qq.com>
2020-01-07 09:26:14 +0800

    efewfe

---
+++ 第十一周/第一周/app/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+	<div id="ppa">
+		<router-link to="/" tag="button">往哪去</router-link>
+		<router-link to="/about" tag="button">上炕都费劲</router-link>
+		<router-link to="/public" tag="button">老实待着得了</router-link>
+		<router-view></router-view>
+	</div>
+</template>
+<script>
+	export default {	
+	}
+</script>
+
+<style scoped>
+
+</style>
---
+++ 第十一周/第一周/app/src/components/about.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>你懂啥是路由吗</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
