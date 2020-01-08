f88a19860207e9c05c6a4e047772c5271bac49f0
suanlin <1402879619@qq.com>
2020-01-08 09:23:35 +0800

    qefubgioe

---
+++ 第十周‘/7/index3.js
@@ -0,0 +1,48 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import BeforEach from '../components/beforeEach.vue'
+Vue.use(VueRouter)
+const routes = [
+  {
+    path: '/',
+    component: BeforEach,
+  },
+  {
+    path:'/b1',
+    component: () => import('../components/b1.vue'),
+    //记录从哪里进来
+    beforeEnter: (to, from, next) => {
+      if(from.fullPath === '/b2' && to.fullPath === '/b1'){
+        next('/');
+        alert('局部')
+      }else{
+        next();
+      }
+    }
+  },
+  {
+    path:'/b2',
+    component: () => import('../components/b2.vue')
+  },
+  {
+    path:'/foo/:id',
+    component: () => import('../components/foo.vue')
+  },
+  {
+    path:'*',
+    component: () => import('../views/404.vue')
+  }
+]
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
+router.beforeEach((to, from, next)=>{
+  if(from.fullPath === '/b2' && to.fullPath === '/b1'){
+    alert('全局')
+  }else{
+    next();
+  }
+    next();
+});
+export default router
